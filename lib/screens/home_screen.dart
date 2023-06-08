6316c3fb1bc3e60c6c9547dc424ffd2e8bba1aa0
almunawar12 <rifkialmunawar12@gmail.com>
2023-06-08 20:26:26 +0700

    add banner in home page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,15 +12,119 @@ class HomeScreen extends StatelessWidget {
       backgroundColor: background,
       appBar: _appBar(),
       bottomNavigationBar: _bottomNavigationBar(),
-      body: SafeArea(child: Text("Home Screen")),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Assalamualaikum",
+              style: GoogleFonts.poppins(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+                color: text,
+              ),
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            Text(
+              "Rifki Almunawar",
+              style: GoogleFonts.poppins(
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            _lastRead(),
+          ],
+        ),
+      ),
     );
   }
 
+  Stack _lastRead() {
+    return Stack(
+            children: [
+              Container(
+                height: 131,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  gradient: const LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      stops: [
+                        0,
+                        0.6,
+                        1
+                      ],
+                      colors: [
+                        Color(0xFFDF98FA),
+                        Color(0xFFB070FD),
+                        Color(0xFF9055FF),
+                      ]),
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                right: 0,
+                child: SvgPicture.asset('assets/svgs/quran.svg'),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        SvgPicture.asset('assets/svgs/book.svg'),
+                        const SizedBox(
+                          width: 8,
+                        ),
+                        Text(
+                          "Last Read",
+                          style: GoogleFonts.poppins(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      "Al-Fatihah",
+                      style: GoogleFonts.poppins(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      "Ayat No: 1",
+                      style: GoogleFonts.poppins(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          );
+  }
+
   BottomNavigationBar _bottomNavigationBar() => BottomNavigationBar(
-    type: BottomNavigationBarType.fixed,
-    backgroundColor: gray,
-    showSelectedLabels: false,
-    showUnselectedLabels: false,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: gray,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
         items: [
           _bottomBar(icon: "assets/svgs/quran-icon.svg", label: "Quran"),
           _bottomBar(icon: "assets/svgs/lamp-icon.svg", label: "Tips"),
@@ -60,7 +164,7 @@ class HomeScreen extends StatelessWidget {
           Text(
             "Quran App",
             style: GoogleFonts.poppins(
-              fontSize: 20,
+              fontSize: 24,
               fontWeight: FontWeight.bold,
             ),
           ),
