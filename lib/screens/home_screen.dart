6ec6eeb5a80e5fec88e12fb8e2b929256e8bd415
almunawar12 <rifkialmunawar12@gmail.com>
2023-06-08 08:16:13 +0700

    add bottom navbar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,41 +8,69 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return _appBar();
-  }
-
-  Scaffold _appBar() {
     return Scaffold(
       backgroundColor: background,
-      appBar: AppBar(
-        backgroundColor: background,
-        elevation: 0,
-        automaticallyImplyLeading: false,
-        title: Row(
-          children: [
-            IconButton(
-              onPressed: () {},
-              icon: SvgPicture.asset('assets/svgs/menu-icon.svg'),
-            ),
-            const SizedBox(
-              width: 24,
-            ),
-            Text(
-              "Quran App",
-              style: GoogleFonts.poppins(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const Spacer(),
-            IconButton(
-              onPressed: () {},
-              icon: SvgPicture.asset('assets/svgs/search-icon.svg'),
-            ),
-          ],
-        ),
-      ),
+      appBar: _appBar(),
+      bottomNavigationBar: _bottomNavigationBar(),
       body: SafeArea(child: Text("Home Screen")),
+    );
+  }
+
+  BottomNavigationBar _bottomNavigationBar() => BottomNavigationBar(
+    type: BottomNavigationBarType.fixed,
+    backgroundColor: gray,
+    showSelectedLabels: false,
+    showUnselectedLabels: false,
+        items: [
+          _bottomBar(icon: "assets/svgs/quran-icon.svg", label: "Quran"),
+          _bottomBar(icon: "assets/svgs/lamp-icon.svg", label: "Tips"),
+          _bottomBar(icon: "assets/svgs/pray-icon.svg", label: "Sholat"),
+          _bottomBar(icon: "assets/svgs/doa-icon.svg", label: "Doa"),
+          _bottomBar(icon: "assets/svgs/bookmark-icon.svg", label: "Bookmark"),
+        ],
+      );
+
+  BottomNavigationBarItem _bottomBar(
+          {required String icon, required String label}) =>
+      BottomNavigationBarItem(
+          icon: SvgPicture.asset(
+            icon,
+            color: text,
+          ),
+          activeIcon: SvgPicture.asset(
+            icon,
+            color: primary,
+          ),
+          label: label);
+
+  AppBar _appBar() {
+    return AppBar(
+      backgroundColor: background,
+      elevation: 0,
+      automaticallyImplyLeading: false,
+      title: Row(
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset('assets/svgs/menu-icon.svg'),
+          ),
+          const SizedBox(
+            width: 24,
+          ),
+          Text(
+            "Quran App",
+            style: GoogleFonts.poppins(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const Spacer(),
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset('assets/svgs/search-icon.svg'),
+          ),
+        ],
+      ),
     );
   }
 }
