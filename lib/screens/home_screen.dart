1fbaea54881bb5e19c63c5304275907faee07f2d
almunawar12 <rifkialmunawar12@gmail.com>
2023-06-08 07:36:04 +0700

    add app bar on home page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:quran_apps/screens/global.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return _appBar();
+  }
+
+  Scaffold _appBar() {
+    return Scaffold(
+      backgroundColor: background,
+      appBar: AppBar(
+        backgroundColor: background,
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        title: Row(
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: SvgPicture.asset('assets/svgs/menu-icon.svg'),
+            ),
+            const SizedBox(
+              width: 24,
+            ),
+            Text(
+              "Quran App",
+              style: GoogleFonts.poppins(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Spacer(),
+            IconButton(
+              onPressed: () {},
+              icon: SvgPicture.asset('assets/svgs/search-icon.svg'),
+            ),
+          ],
+        ),
+      ),
       body: SafeArea(child: Text("Home Screen")),
     );
   }
